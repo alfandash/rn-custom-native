3da3634086bcf143f0b9486d11b4f7ec513829b7
alfandash <cumiasem91@gmail.com>
2022-10-06 00:03:45 +0700

    initial commit react native typescript

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
+  rules: {
+    'react-native/no-inline-styles': 'off',
+    'react/no-unstable-nested-components': 'off',
+  },
+};
