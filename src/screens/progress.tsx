def49c0f6c148db99efcc8bfda1b7e6cef2c1c87
alfandash <cumiasem91@gmail.com>
2022-10-06 11:18:45 +0700

    progress bar with hold function

--- src/screens/progress.tsx
+++ src/screens/progress.tsx
@@ -1,20 +1,41 @@
-import { Text, Box, Center } from 'native-base';
+import { Box, Center, Progress } from 'native-base';
+import { useRef, useState, useEffect } from 'react';
 
 function ProgressScreen() {
+  const countInterval = useRef<number | undefined>(undefined);
+  const [count, setCount] = useState(0);
+  const [hold, setHold] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!hold) {
+      countInterval.current = setInterval(
+        () => setCount((latest) => latest + 1),
+        10,
+      );
+    }
+
+    return () => {
+      clearInterval(countInterval.current);
+    };
+  }, [hold]);
+
+  useEffect(() => {
+    if (count >= 100) {
+      setCount(0);
+    }
+  }, [count]);
   return (
-    <Center flex={1}>
+    <Center flex={1} w="100%">
       <Box
-        p="4"
-        bg={'black'}
-        maxW="300"
-        w="100%"
-        mt={10}
-        safeArea
-        color={'white'}
+        w="90%"
+        maxW="400"
+        onTouchStart={() => setHold(true)}
+        onTouchEnd={() => setHold(false)}
       >
-        <Text fontSize="sm" display="flex" color={'white'}>
-          Pogress
-        </Text>
+        <Progress value={count} mx="4" />
+        <Box w="100%" alignItems={'center'}>
+          {`${count} %`}
+        </Box>
       </Box>
     </Center>
   );
